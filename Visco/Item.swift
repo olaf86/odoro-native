b6f4679113fa7dbb526d149574148b8b2d55f150
olaf86 <yogawa0417@gmail.com>
2026-04-07 22:00:59 +0900

    Initial Commit

---
+++ Visco/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Visco
+//
+//  Created by Yuta Ogawa on 2026/04/07.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Visco/ViscoApp.swift
@@ -0,0 +1,32 @@
+//
+//  ViscoApp.swift
+//  Visco
+//
+//  Created by Yuta Ogawa on 2026/04/07.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ViscoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
